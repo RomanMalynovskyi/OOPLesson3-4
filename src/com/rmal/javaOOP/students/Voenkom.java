dd025a711d02f20c3edf303e6d64c1f451278cae
RomanMalynovskyi <malinovskiir@mail.ru>
2017-01-28 01:51:19 +0200

    Lesson4

---
+++ src/com/rmal/javaOOP/students/Voenkom.java
@@ -0,0 +1,7 @@
+package com.rmal.javaOOP.students;
+
+// 4.Реализуйте интерфейс Военком которые вернет из группы массив студентов юношей которым больше 18 лет.
+
+interface Voenkom {
+    Group getStudentForArmy(Group group);
+}
